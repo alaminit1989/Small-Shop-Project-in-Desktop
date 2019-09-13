291969d76863d8e08d126db3c2ee4a617a85ef06
said <aid@AGCORP-WS17.com>
2019-09-13 18:08:35 +0600

    data validation

---
+++ BLL/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCM.BLL
+{
+  public  class Customer
+    {
+        public void CreateCustomer(string strCustomerName,string strMobileNo,string strAddress,decimal creditBlance,decimal openningBalance,int intUnitId)
+        {
+
+        }
+        public DataTable GetCustomer(int intCustId, string strmMobileNo)
+        {
+            return new DataTable();
+        }
+        public DataTable  GetCustomerAll(int intUnitId)
+        {
+            return new DataTable();
+
+
+        }
+    }
+}
